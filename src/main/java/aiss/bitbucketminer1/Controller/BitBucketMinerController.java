4d4291e7066bb3a8d66b821f689183a2308acf31
Santiago <sanrodper@alum.us.es>
2025-05-11 22:41:44 +0200

    DOC (No he tocado nada que no es mio)

--- src/main/java/aiss/bitbucketminer1/Controller/BitBucketMinerController.java
+++ src/main/java/aiss/bitbucketminer1/Controller/BitBucketMinerController.java
@@ -1,24 +1,39 @@
 package aiss.bitbucketminer1.Controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import aiss.bitbucketminer1.etl.BitbucketTransformer;
 import aiss.bitbucketminer1.model.BitBucket.comment.CommentJava;
 import aiss.bitbucketminer1.model.BitBucket.commit.CommitJava;
 import aiss.bitbucketminer1.model.BitBucket.issues.IssuesJava;
+import aiss.bitbucketminer1.model.BitBucket.project.ProjectJava;
 import aiss.bitbucketminer1.model.GitMiner.Issue;
 import aiss.bitbucketminer1.model.GitMiner.Project;
-import aiss.bitbucketminer1.model.BitBucket.project.ProjectJava;
-import aiss.bitbucketminer1.service.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
+import aiss.bitbucketminer1.service.CommentService;
+import aiss.bitbucketminer1.service.CommitService;
+import aiss.bitbucketminer1.service.IssuesService;
+import aiss.bitbucketminer1.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+@Tag(name = "BitBucketMiner", description = "BitBucket data collector API")
 @RestController
 public class BitBucketMinerController {
 
@@ -43,13 +58,24 @@ public class BitBucketMinerController {
     final String gitMinerApiUrl = "http://localhost:8081/bitbucketminer/v1/projects";
 
     @GetMapping("/bitbucket/{workspace}/{repo}")
+    @Operation(
+            summary = "Retrieves data from BitBucket",
+            description = "Creates a project from BitBucket's data",
+            tags ={"Projects","GET"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",content = {@Content(schema = @Schema(implementation = Project.class))}),
+            @ApiResponse(responseCode = "404",content = {@Content(schema = @Schema())})
+    })
+
     public ResponseEntity<Project> getRepositoryData(
             @PathVariable String workspace,
             @PathVariable String repo,
+            @Parameter(description = "number of commits,default 5")
             @RequestParam(defaultValue = "5") Integer nCommits,
+            @Parameter(description = "number of issues,default 5")
             @RequestParam(defaultValue = "5") Integer nIssues,
-            @RequestParam(defaultValue = "2") Integer maxPages
-    ) {
+            @Parameter(description = "maximum of pages,default 2")
+            @RequestParam(defaultValue = "2") Integer maxPages) {
         // Get raw data from services
         ProjectJava bitbucketProject = projectService.getProject(workspace, repo);
         List<CommitJava> bitbucketCommits = commitService.getNCommits(workspace, repo, nCommits, maxPages);
@@ -82,13 +108,23 @@ public class BitBucketMinerController {
     }
 
     @PostMapping("/bitbucket/{workspace}/{repo}")
+   @Operation(
+            summary = "Obtains and resends data to another endpoint or backend",
+            description = "Creates a project from BitBucket's data and sends it",
+            tags ={"Projects","POST"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",content = {@Content(schema = @Schema(implementation = Project.class))}),
+            @ApiResponse(responseCode = "404",content = {@Content(schema = @Schema())})
+    })
     public ResponseEntity<Project> sendRepositoryData(
             @PathVariable String workspace,
             @PathVariable String repo,
+            @Parameter(description = "number of commits,default 5")
             @RequestParam(defaultValue = "5") Integer nCommits,
+            @Parameter(description = "number of issues,default 5")
             @RequestParam(defaultValue = "5") Integer nIssues,
-            @RequestParam(defaultValue = "2") Integer maxPages
-    ) {
+            @Parameter(description = "maximum of pages,default 2")
+            @RequestParam(defaultValue = "2") Integer maxPages) {
         // Get and transform data (same as GET endpoint)
         ResponseEntity<Project> response = getRepositoryData(
                 workspace, repo, nCommits, nIssues, maxPages
@@ -106,7 +142,13 @@ public class BitBucketMinerController {
 
         return ResponseEntity.ok(gitMinerResponse.getBody());
     }
-
+    @Operation(
+            summary = "Obtains a Project",
+            tags ={"Projects","POST"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",content = {@Content(schema = @Schema(implementation = Project.class))}),
+            @ApiResponse(responseCode = "404",content = {@Content(schema = @Schema())})
+    })
     @PostMapping("/bitbucketminer/v1/projects")
     public ResponseEntity<Project> receiveProject(@RequestBody Project project) {
         System.out.println("Project received: " + project.getName());
